a28ae71afc4c7f22ec3235349d6631d070471d5c
James Wilson <jamestwilson@live.com>
2019-09-21 13:53:55 -0700

    Working nav bar

---
+++ article.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="assets/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="topnav">
+            <a href="index.php">Index</a>
+            <a href="articles.php">articles</a>
+            <a href="login.php">login</a>
+            <a href="register.php">register</a>
+            <a class="active" href="#">article</a>
+            <a href="profile.php">profile</a>
+        </div>
+        <h1>Article Page</h1>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="assets/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        
+        <div class="topnav">
+            <a class="active" href="#">Index</a>
+            <a href="articles.php">articles</a>
+            <a href="login.php">login</a>
+            <a href="register.php">register</a>
+            <a href="article.php">article</a>
+            <a href="profile.php">profile</a>
+        </div>
+        
+        <h1>Index Page</h1>
+        
+        <?php       
+                
+        ?>
+    </body>
+</html>
